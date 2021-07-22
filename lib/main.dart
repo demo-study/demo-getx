1ce81ee91c9aacfc60f2882cbde8d4c46e6d7502
lulin <2445530702@qq.com>
2021-07-22 17:27:01 +0800

    1

--- lib/main.dart
+++ lib/main.dart
@@ -25,17 +25,26 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      // 关闭debugger标志
       debugShowCheckedModeBanner: false,
+      // 日志
       enableLog: true,
       logWriterCallback: Logger.write,
+      // 初始路由
       initialRoute: AppPages.INITIAL,
+      // 路由
       getPages: AppPages.routes,
+      // 找不到路由
       unknownRoute: AppPages.unknownRoute,
       // locale: TranslationService.locale,
       // fallbackLocale: TranslationService.fallbackLocale,
       // translations: TranslationService(),
+      // 多语言
+      // 初始
       locale: TranslationService.locale,
+      // 找不到的话
       fallbackLocale: TranslationService.fallbackLocale,
+      // 字典
       translations: TranslationService(),
     );
   }
